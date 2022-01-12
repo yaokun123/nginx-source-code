5418514d79477a978af5096184eed519880d3d23
姚坤 <yaok@pingansec.com>
2022-01-12 18:56:59 +0800

    实现后台守护进程

--- src/os/unix/ngx_daemon.c
+++ src/os/unix/ngx_daemon.c
@@ -8,33 +8,43 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+//// 实现守护进程
 ngx_int_t
 ngx_daemon(ngx_log_t *log)
 {
     int  fd;
 
+    //// fork子进程
     switch (fork()) {
     case -1:
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "fork() failed");
         return NGX_ERROR;
 
     case 0:
+        //// 子进程继续往下执行
         break;
 
     default:
+        //// 父进程直接退出 - 因为父进程不能实现守护进程
         exit(0);
     }
 
+
+    //// 以下都是子进程的操作
     ngx_pid = ngx_getpid();
 
+    //// 子进程创建会话id，实现守护进程
+    //// 让父进程退出原因就是为了这一步，因为父进程是进程组组长，而进程组组长不能创建会话id
     if (setsid() == -1) {
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "setsid() failed");
         return NGX_ERROR;
     }
 
+    //// 设置文件掩码
     umask(0);
 
+
+    //// 设置输入、输出文件描述符指向空洞文件
     fd = open("/dev/null", O_RDWR);
     if (fd == -1) {
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno,
@@ -42,11 +52,13 @@ ngx_daemon(ngx_log_t *log)
         return NGX_ERROR;
     }
 
+    //// 输入
     if (dup2(fd, STDIN_FILENO) == -1) {
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "dup2(STDIN) failed");
         return NGX_ERROR;
     }
 
+    //// 输出
     if (dup2(fd, STDOUT_FILENO) == -1) {
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "dup2(STDOUT) failed");
         return NGX_ERROR;
